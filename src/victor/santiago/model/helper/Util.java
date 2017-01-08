a0ad76028847efc11d2240e7574d1594b60efa35
Victor D. S. <vdsantiago.1@gmail.com>
2017-01-07 22:16:20 -0200

    Date methods

---
+++ src/victor/santiago/model/helper/Util.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Victor Santiago.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package victor.santiago.model.helper;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Collection of Helper methods
+ *
+ * @author Victor Santiago
+ */
+public class Util {
+    
+    public static String readFile(String path) 
+            throws IOException{
+        return readFile(path, StandardCharsets.UTF_8);
+    }
+ 
+    public static String readFile(String path, Charset encoding) 
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+    
+    public static Date addOneDayToDate(Date date) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+        
+        return c.getTime();
+    }
+    
+    public static String getDateAsString(Date d) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        return df.format(d);
+    }
+}
